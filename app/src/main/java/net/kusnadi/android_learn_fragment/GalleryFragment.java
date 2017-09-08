97f09b99aac3754c28d707bc6009c6f88f044dc3
Kusnadi <kusnadi@nutech-integrasi.com>
2017-09-08 23:33:28 +0700

    first commit

---
+++ app/src/main/java/net/kusnadi/android_learn_fragment/GalleryFragment.java
@@ -0,0 +1,25 @@
+package net.kusnadi.android_learn_fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by root on 08/09/17.
+ */
+
+public class GalleryFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.gallery_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
